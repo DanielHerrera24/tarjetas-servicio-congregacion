f1a9f79335cd5c60a351704ba69bbc406acc840b
Daniel Herrera <info.danielherrera@gmail.com>
2025-01-15 14:35:51 -0700

    CopiarIDs añado copiar nombres y loading al pulsar ver IDs

--- src/components/CopiarIDsModal.jsx
+++ src/components/CopiarIDsModal.jsx
@@ -4,17 +4,21 @@ import { toast } from "react-toastify";
 import { useDarkMode } from "../context/DarkModeContext";
 import { IoCopy } from "react-icons/io5";
 import { collection, getDocs } from "firebase/firestore";
+import { SyncLoader } from "react-spinners";
 
 const CopiarIDsModal = ({ db, congregacionId }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [grupos, setGrupos] = useState({});
   const [grupoActual, setGrupoActual] = useState(null);
   const { darkMode } = useDarkMode();
+  const [loading, setLoading] = useState(false);
 
   // Función para abrir el modal
   const openModal = async () => {
-    await fetchGrupos();
     setIsModalOpen(true);
+    setLoading(true);
+    await fetchGrupos();
+    setLoading(false);
   };
 
   // Función para cerrar el modal
@@ -44,7 +48,10 @@ const CopiarIDsModal = ({ db, congregacionId }) => {
         );
         gruposData[grupoId] = {
           nombre: grupoNombre,
-          ids: hermanosSnapshot.docs.map((doc) => doc.id),
+          hermanos: hermanosSnapshot.docs.map((doc) => ({
+            id: doc.id,
+            nombre: doc.data().nombre || "Sin Nombre",
+          })),
         };
       }
 
@@ -66,21 +73,49 @@ const CopiarIDsModal = ({ db, congregacionId }) => {
   };
 
   // Función para copiar los IDs del grupo actual al portapapeles
-  const copiarAlPortapapeles = () => {
-    const ids = grupos[grupoActual]?.ids.join("\n") || "";
-    navigator.clipboard.writeText(ids).then(() => {
-      toast.success(`IDs copiados al portapapeles.`, {
-        position: "bottom-center",
-        autoClose: 3000,
-        hideProgressBar: true,
-        closeOnClick: true,
-        draggable: true,
-        theme: darkMode ? "dark" : "light",
-        style: {
-          border: darkMode ? "1px solid #ffffff" : "1px solid #000000",
-        },
+  const copiarAlPortapapeles = (tipo) => {
+    const datos =
+      tipo === "nombre"
+        ? grupos[grupoActual]?.hermanos.map(({ nombre }) => nombre).join("\n")
+        : grupos[grupoActual]?.hermanos.map(({ id }) => id).join("\n");
+
+    if (datos) {
+      navigator.clipboard.writeText(datos).then(() => {
+        toast.success(
+          tipo === "nombre"
+            ? "Nombres copiados al portapapeles."
+            : "IDs copiados al portapapeles.",
+          {
+            position: "bottom-center",
+            autoClose: 3000,
+            hideProgressBar: true,
+            closeOnClick: true,
+            draggable: true,
+            theme: darkMode ? "dark" : "light",
+            style: {
+              border: darkMode ? "1px solid #ffffff" : "1px solid #000000",
+            },
+          }
+        );
       });
-    });
+    } else {
+      toast.error(
+        tipo === "nombre"
+          ? "No hay nombres para copiar."
+          : "No hay IDs para copiar.",
+        {
+          position: "bottom-center",
+          autoClose: 3000,
+          hideProgressBar: true,
+          closeOnClick: true,
+          draggable: true,
+          theme: darkMode ? "dark" : "light",
+          style: {
+            border: darkMode ? "1px solid #ffffff" : "1px solid #000000",
+          },
+        }
+      );
+    }
   };
 
   return (
@@ -98,49 +133,77 @@ const CopiarIDsModal = ({ db, congregacionId }) => {
       {isModalOpen && (
         <div className="fixed inset-0 bg-gray-800 bg-opacity-75 flex justify-center items-center z-50">
           <div
-            className={`p-6 rounded shadow-lg max-w-lg w-full ${
+            className={`p-6 rounded-2xl shadow-lg max-w-lg ${
+              loading ? "w-52" : "w-full"
+            } ${
               darkMode
                 ? "bg-[#1f1f1f] border-white text-white shadow-slate-600"
                 : "bg-white border-black text-black"
             }`}
           >
-            <h2 className="text-xl font-bold mb-4">IDs por Grupo</h2>
-            <div className="flex mb-4 overflow-x-auto space-x-2">
-              {Object.keys(grupos).map((grupoId) => (
-                <button
-                  key={grupoId}
-                  onClick={() => setGrupoActual(grupoId)}
-                  className={`px-4 py-2 rounded min-w-28 ${
-                    grupoActual === grupoId
-                      ? "bg-green-500 text-white"
-                      : "bg-gray-300 text-black hover:bg-gray-400"
-                  }`}
-                >
-                  {grupos[grupoId].nombre}
-                </button>
-              ))}
-            </div>
-            <ul className="mb-4 max-h-[338px] sm:max-h-[512px] overflow-y-scroll">
-              {grupos[grupoActual]?.ids.map((id) => (
-                <li key={id} className="border-b text-left">
-                  {id}
-                </li>
-              )) || <p>No hay IDs para este grupo.</p>}
-            </ul>
-            <div className="flex justify-end space-x-2">
-              <button
-                onClick={copiarAlPortapapeles}
-                className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600"
-              >
-                Copiar IDs
-              </button>
-              <button
-                onClick={closeModal}
-                className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600"
-              >
-                Cerrar
-              </button>
-            </div>
+            {loading ? (
+              <div className="flex justify-center items-center h-32">
+                <SyncLoader color="#3B82F6" />
+              </div>
+            ) : (
+              <>
+                <h2 className="text-xl font-bold mb-4">IDs por Grupo</h2>
+                <div className="flex mb-4 overflow-x-auto space-x-2">
+                  {Object.keys(grupos).map((grupoId) => (
+                    <button
+                      key={grupoId}
+                      onClick={() => setGrupoActual(grupoId)}
+                      className={`px-4 py-2 rounded min-w-28 ${
+                        grupoActual === grupoId
+                          ? "bg-green-500 text-white"
+                          : "bg-gray-300 text-black hover:bg-gray-400"
+                      }`}
+                    >
+                      {grupos[grupoId].nombre}
+                    </button>
+                  ))}
+                </div>
+                <ul className="mb-4 grid grid-cols-2 gap-4 max-h-[338px] sm:max-h-[512px] overflow-y-scroll">
+                  <div>
+                    <h3 className="font-semibold mb-2">Nombres</h3>
+                    {grupos[grupoActual]?.hermanos.map(({ id, nombre }) => (
+                      <li key={id} className="border-b text-left">
+                        {nombre}
+                      </li>
+                    )) || <p>No hay hermanos en este grupo.</p>}
+                  </div>
+                  <div>
+                    <h3 className="font-semibold mb-2">IDs</h3>
+                    {grupos[grupoActual]?.hermanos.map(({ id }) => (
+                      <li key={id} className="border-b text-left">
+                        {id}
+                      </li>
+                    )) || <p>No hay IDs en este grupo.</p>}
+                  </div>
+                </ul>
+
+                <div className="flex justify-between space-x-2">
+                  <button
+                    onClick={() => copiarAlPortapapeles("nombre")}
+                    className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
+                  >
+                    Copiar Nombres
+                  </button>
+                  <button
+                    onClick={() => copiarAlPortapapeles("id")}
+                    className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600"
+                  >
+                    Copiar IDs
+                  </button>
+                  <button
+                    onClick={closeModal}
+                    className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600"
+                  >
+                    Cerrar
+                  </button>
+                </div>
+              </>
+            )}
           </div>
         </div>
       )}
